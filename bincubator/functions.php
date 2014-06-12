063e9db5ddc06ac1d37e2b8ae0d7aca2b668ea58
Robert Ramey <ramey@rrsd.com>
2014-06-12 07:58:55 -0700

    added RSS feed

--- bincubator/functions.php
+++ bincubator/functions.php
@@ -1,5 +1,29 @@
 <?php
 
+/*
+ * remove items from admin menu - not currently used
+ */
+add_action( 'admin_menu', 'remove_links_menu' );
+function remove_links_menu() {
+     remove_menu_page('index.php'); // Dashboard
+     remove_menu_page('edit.php'); // Posts
+     remove_menu_page('upload.php'); // Media
+     remove_menu_page('link-manager.php'); // Links
+     remove_menu_page('edit.php?post_type=page'); // Pages
+     remove_menu_page('edit-comments.php'); // Comments
+     remove_menu_page('themes.php'); // Appearance
+     remove_menu_page('plugins.php'); // Plugins
+     remove_menu_page('users.php'); // Users
+     remove_menu_page('tools.php'); // Tools
+     remove_menu_page('options-general.php'); // Settings
+}
+
+/*
+ * only permit admins to see the admin bar
+ */
+if (!current_user_can('manage_options')) {
+	add_filter('show_admin_bar', '__return_false');
+}
 
 /*
  * prevent any submissions from unregistered users
