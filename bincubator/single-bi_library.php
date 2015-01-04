99950d5e78f4874e4d63a4ded89f0d0a41098778
Robert Ramey <ramey@rrsd.com>
2015-01-03 21:13:30 -0800

    Finally fixed library page edit

--- bincubator/single-bi_library.php
+++ bincubator/single-bi_library.php
@@ -22,42 +22,45 @@ add_filter('gform_update_post/public_edit', '__return_true');
 <script type="text/javascript">
 	jQuery(document).ready(function($){
         $('textarea').autosize();
-		$('#respond').prop('hidden', true);
-		$('.comment').prop('hidden', true);
-		$('#comments_button').click(function(event) {
-			var hide = ! $('#respond').prop('hidden');
-			$('#respond').prop('hidden',hide);
-			$('.comment').prop('hidden',hide);
-		});
-        // author is never editable
-        $('.gform_wrapper #input_1_17').prop('readonly', true);
-        // library name is never editiable
-        $('.gform_wrapper #input_1_1').prop('readonly', 'true');
-        <?php if(! is_editable()){ ?>
-			$('#gform_submit_button_1').prop('hidden', 'true');
-        <?php } ?>
-		var edit_mode;
-		function set_edit_mode(mode){
+        $('#respond').prop('hidden', true);
+        $('.comment').prop('hidden', true);
+        $('#comments_button').click(function(event) {
+            var hide = ! $('#respond').prop('hidden');
+            $('#respond').prop('hidden',hide);
+            $('.comment').prop('hidden',hide);
+        });
+        $('#gform_submit_button_1').val('Update');
+        var edit_mode;
+        function set_edit_mode(mode){
+            // author library names are never editable
+            $('.gform_body #input_1_17').prop('readonly', true);
+            $('.gform_body #input_1_1').prop('readonly', true);
+
             // mode true => permit editing
             // mode false => don't permit editing
             $('.gform_body').prop('readonly', ! mode);
-            $('#gform_submit_button_1').val(mode ? 'Update' : 'Edit');
-            $('.gform_wrapper textarea').prop('readonly', ! mode);
+            $('.gform_body textarea').prop('readonly', ! mode);
+            $('.gform_body input').prop('readonly', ! mode);
 
-			$('.ginput_container input').prop('readonly', ! mode);
-            $('.ginput_container textarea').prop('readonly', ! mode);
-            //$('.gform_wrapper input').prop('disabled', ! mode);
+            // only show edit button when we actually edit
+            $('#gform_submit_button_1').prop('hidden', ! mode);
+
+            $('#edit_button').html(mode ? 'Cancel' : 'Edit');
+
+            // adjust cursor according to mode
             $('.library_link input').css('cursor', mode ? 'auto' : 'pointer');
             // sponsor list is only visible when editing
             $('.library_visible').css('display', mode ? 'block' : 'none');
-			edit_mode = mode;
-		}
+            edit_mode = mode;
+        }
         set_edit_mode(false);
-		// setup handler for edit/view button
-		$('form').submit(function(event){
+        $('#edit_button').click(function() {
+            // toggle to new edit mode
             set_edit_mode(! edit_mode);
-            return ! edit_mode;
-		});
+        });
+        $('form').submit(function() {
+            set_edit_mode(false);
+        });
         $('.library_link').click(function($) {
             if(! edit_mode){
                 window.open(this.children[1].firstChild.value);
@@ -94,6 +97,7 @@ add_filter('gform_update_post/public_edit', '__return_true');
         do_action('gform_update_post/setup_form', $post_id);
         gravity_form(1);
         ?>
+        <button class="blincubator_button" id="edit_button">Edit</button><br/>
 		<a class="blincubator_button" id="statistics_button" href="http://rrsd.com/wordpresstest/wp-admin/admin.php?page=wp-slim-view-3&fs[user]=is_not_equal_to+<?php echo get_userdata($post->post_author)->user_login;?>&fs[type]=is_not_equal_to+1&fs[resource]=contains+<?php echo $post->post_name;?>">Display Statistics</a>
         <br/>
 		<a class="blincubator_button" id="reviews_button" href="reviews?library_id=<?php the_ID(); ?>">Reviews</a>
@@ -101,7 +105,7 @@ add_filter('gform_update_post/public_edit', '__return_true');
         $reviews = get_reviews_query($post->ID);
         echo "There are " . $reviews->found_posts . " reviews";
         ?>
-        <br>
+        <br/>
         <button class="blincubator_button" id="comments_button">Show/Hide Comments</button>
         <?php
         $comments = get_comments(array(
